a70dd638352d6508bda9374cb740edbf85e4bf85
Kevin Ready <kevin.ready@gmail.com>
2023-01-04 10:43:36 -0800

    Added render.com fetch address

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
 	loader(messageDiv);
 
-	const response = await fetch("http://localhost:7070", {
+	const response = await fetch("https://our-bitch.onrender.com", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
